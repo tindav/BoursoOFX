bd25306b24346467b505c3dee711f844c21382cc
David Rozen <drozen@microsoft.com>
2025-04-02 14:08:35 +0200

    Adding Drag and Drop feature

--- Converter.UI/MainWindow.Designer.cs
+++ Converter.UI/MainWindow.Designer.cs
@@ -30,11 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             sourceFileDialog = new OpenFileDialog();
-            SourceFileName = new TextBox();
             destinationFileDialog = new SaveFileDialog();
+            dragZone = new Label();
             OpenFileButton = new Button();
-            label1 = new Label();
             convertButton = new Button();
+            label1 = new Label();
+            SourceFileName = new TextBox();
             SuspendLayout();
             // 
             // sourceFileDialog
@@ -42,58 +43,73 @@
             sourceFileDialog.FileName = "*.ofx";
             sourceFileDialog.Filter = "OFX Files|*.ofx;*.qfx|All files|*.*";
             // 
-            // SourceFileName
-            // 
-            SourceFileName.Location = new Point(128, 66);
-            SourceFileName.Name = "SourceFileName";
-            SourceFileName.ReadOnly = true;
-            SourceFileName.Size = new Size(331, 27);
-            SourceFileName.TabIndex = 0;
-            SourceFileName.Click += OpenFileButton_Click;
-            // 
             // destinationFileDialog
             // 
             destinationFileDialog.DefaultExt = "ofx";
             destinationFileDialog.FileName = "money.ofx";
             destinationFileDialog.Filter = "OFX File|*.ofx|All files|*.*";
             // 
+            // dragZone
+            // 
+            dragZone.AllowDrop = true;
+            dragZone.Dock = DockStyle.Top;
+            dragZone.Font = new Font("Segoe UI", 24F);
+            dragZone.Location = new Point(0, 0);
+            dragZone.Name = "dragZone";
+            dragZone.Size = new Size(597, 56);
+            dragZone.TabIndex = 8;
+            dragZone.Text = "Drop your file here ...";
+            dragZone.TextAlign = ContentAlignment.TopCenter;
+            dragZone.DragDrop += dragZone_DragDrop;
+            dragZone.DragOver += dragZone_DragOver;
+            dragZone.DragLeave += dragZone_DragLeave;
+            // 
             // OpenFileButton
             // 
-            OpenFileButton.Location = new Point(465, 65);
+            OpenFileButton.Location = new Point(478, 74);
             OpenFileButton.Name = "OpenFileButton";
             OpenFileButton.Size = new Size(94, 29);
-            OpenFileButton.TabIndex = 1;
+            OpenFileButton.TabIndex = 10;
             OpenFileButton.Text = "&Browse...";
             OpenFileButton.UseVisualStyleBackColor = true;
             OpenFileButton.Click += OpenFileButton_Click;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 70);
-            label1.Name = "label1";
-            label1.Size = new Size(112, 20);
-            label1.TabIndex = 2;
-            label1.Text = "Source OFX File";
-            // 
             // convertButton
             // 
-            convertButton.Location = new Point(12, 93);
+            convertButton.Location = new Point(25, 109);
             convertButton.Name = "convertButton";
             convertButton.Size = new Size(547, 29);
-            convertButton.TabIndex = 3;
+            convertButton.TabIndex = 11;
             convertButton.Text = "&Convert";
             convertButton.UseVisualStyleBackColor = true;
             convertButton.Click += convertButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 20);
+            label1.TabIndex = 12;
+            label1.Text = "Source OFX File";
+            // 
+            // SourceFileName
+            // 
+            SourceFileName.Location = new Point(135, 76);
+            SourceFileName.Name = "SourceFileName";
+            SourceFileName.ReadOnly = true;
+            SourceFileName.Size = new Size(337, 27);
+            SourceFileName.TabIndex = 9;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(597, 163);
+            Controls.Add(dragZone);
+            Controls.Add(OpenFileButton);
             Controls.Add(convertButton);
             Controls.Add(label1);
-            Controls.Add(OpenFileButton);
             Controls.Add(SourceFileName);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Icon = (Icon)resources.GetObject("$this.Icon");
@@ -108,10 +124,11 @@
         #endregion
 
         private OpenFileDialog sourceFileDialog;
-        private TextBox SourceFileName;
         private SaveFileDialog destinationFileDialog;
+        private Label dragZone;
         private Button OpenFileButton;
-        private Label label1;
         private Button convertButton;
+        private Label label1;
+        private TextBox SourceFileName;
     }
 }
--- Converter.UI/MainWindow.cs
+++ Converter.UI/MainWindow.cs
@@ -20,8 +20,14 @@ namespace Converter.UI
 
         private void convertButton_Click(object sender, EventArgs e)
         {
+            if (!CheckFileExtension(SourceFileName.Text))
+            {
+                SourceFileName.Text = "";
+                return;
+            }
+
             destinationFileDialog.FileName = Path.GetFileNameWithoutExtension(SourceFileName.Text) + ".converted.ofx";
-            var saveFileResult =  destinationFileDialog.ShowDialog();
+            var saveFileResult = destinationFileDialog.ShowDialog();
             if (saveFileResult != DialogResult.OK)
             {
                 return;
@@ -32,6 +38,55 @@ namespace Converter.UI
             File.WriteAllText(destinationFileDialog.FileName, convertedOfx);
 
             MessageBox.Show("Conversion completed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool CheckFileExtension(string filename)
+        {
+            if (Path.GetExtension(filename) != ".ofx")
+            {
+                MessageBox.Show("Please select a valid OFX file.", "Invalid File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dragZone.BorderStyle = BorderStyle.None;
+                return false;
+            }
+            return true;
+        }
+
+        private void dragZone_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data?.GetDataPresent(DataFormats.FileDrop) == true)
+            {
+                e.Effect = DragDropEffects.Copy;
+                dragZone.BorderStyle = BorderStyle.Fixed3D;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+                dragZone.BorderStyle = BorderStyle.None;
+            }
+        }
+
+        private void dragZone_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data?.GetDataPresent(DataFormats.FileDrop) == true)
+            {
+                string[]? files = e.Data.GetData(DataFormats.FileDrop) as string[];
+                if (files?.Length > 0)
+                {
+                    if (!CheckFileExtension(files[0]))
+                    {
+                        return;
+                    }
+
+                    SourceFileName.Text = files[0];
+                }
+            }
+
+            dragZone.BorderStyle = BorderStyle.None;
+        }
+
+        private void dragZone_DragLeave(object sender, EventArgs e)
+        {
+            dragZone.BorderStyle = BorderStyle.None;
         }
     }
 }
